2d32fefd8ad9784ea01b3fdf76823a16191b7d87
Tyler Schloesser <tylerschloesser@gmail.com>
2024-11-25 15:32:34 -0700

    add prettier

--- README.md
+++ README.md
@@ -18,7 +18,10 @@ export default tseslint.config({
   languageOptions: {
     // other options...
     parserOptions: {
-      project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      project: [
+        './tsconfig.node.json',
+        './tsconfig.app.json',
+      ],
       tsconfigRootDir: import.meta.dirname,
     },
   },
--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
+import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,15 +10,25 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img
+            src={viteLogo}
+            className="logo"
+            alt="Vite logo"
+          />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
         <p>
