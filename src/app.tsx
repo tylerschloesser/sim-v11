968d7e541f0c2722858b99243ffe5d1d048c6ba5
Tyler Schloesser <tylerschloesser@gmail.com>
2024-11-25 16:29:00 -0700

    set viewport

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { Application } from 'pixi.js'
 import { useEffect, useRef } from 'react'
 import invariant from 'tiny-invariant'
+import { Vec2 } from './vec2'
 
 export function App() {
   const container = useRef<HTMLDivElement>(null)
@@ -9,16 +10,38 @@ export function App() {
     invariant(container.current)
 
     const canvas = document.createElement('canvas')
+    canvas.style.width = '100%'
+    canvas.style.height = '100%'
+    canvas.style.display = 'block'
     container.current.appendChild(canvas)
 
+    const viewport = new Vec2(
+      window.innerWidth,
+      window.innerHeight,
+    )
+
     const app = new Application()
-    app.init({ canvas }).then(() => {
-      console.log('after init!')
-    })
+    app
+      .init({
+        canvas,
+        width: viewport.x,
+        height: viewport.y,
+      })
+      .then(() => {
+        console.log('after init!')
+      })
 
     return () => {
       canvas.remove()
     }
   }, [])
-  return <div ref={container} />
+  return (
+    <div
+      style={{
+        width: '100dvw',
+        height: '100dvh',
+      }}
+      ref={container}
+    />
+  )
 }
