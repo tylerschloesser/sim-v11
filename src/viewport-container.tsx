8a0737d5d65415fffe5ad9f0737fe0a61928a482
Tyler Schloesser <tylerschloesser@gmail.com>
2024-11-30 18:45:43 -0800

    refactor viewport container

---
+++ src/viewport-container.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from 'react'
+import invariant from 'tiny-invariant'
+import { Vec2 } from './vec2'
+
+export function ViewportContainer({
+  children,
+  className,
+}: {
+  children: (viewport: Vec2 | null) => React.ReactNode
+  className?: string
+}) {
+  const ref = useRef<HTMLDivElement>(null)
+  const [viewport, setViewport] = useState<Vec2 | null>(
+    null,
+  )
+  useEffect(() => {
+    const ro = new ResizeObserver(() => {
+      invariant(ref.current)
+      const rect = ref.current.getBoundingClientRect()
+      setViewport(new Vec2(rect.width, rect.height))
+    })
+    invariant(ref.current)
+    ro.observe(ref.current)
+    return () => {
+      ro.disconnect()
+    }
+  }, [])
+  return (
+    <div ref={ref} className={className}>
+      {children(viewport)}
+    </div>
+  )
+}
