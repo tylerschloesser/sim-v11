318a632cbaba468a0162f3a4d57b21c790473236
Tyler Schloesser <tylerschloesser@gmail.com>
2024-11-30 16:04:13 -0800

    add types

--- src/game.ts
+++ src/game.ts
@@ -4,10 +4,12 @@ import { ZVec2 } from './vec2'
 const NodeRef = z.strictObject({
   id: z.string(),
 })
+type NodeRef = z.infer<typeof NodeRef>
 
 const NodeItem = z.strictObject({
   tick: z.number(),
 })
+type NodeItem = z.infer<typeof NodeItem>
 
 const Node = z.strictObject({
   id: z.string(),
@@ -15,3 +17,4 @@ const Node = z.strictObject({
   item: NodeItem.nullable(),
   out: NodeRef.array(),
 })
+type Node = z.infer<typeof Node>
