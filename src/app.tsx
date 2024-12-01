5e4bd287dc83012a9560576d713915de7458ec2f
Tyler Schloesser <tylerschloesser@gmail.com>
2024-11-30 18:48:48 -0800

    render rect

--- src/app.tsx
+++ src/app.tsx
@@ -2,13 +2,25 @@ import { Vec2 } from './vec2'
 import { ViewportContainer } from './viewport-container'
 
 function Canvas({ viewport }: { viewport: Vec2 }) {
-  return <>{JSON.stringify(viewport)}</>
+  const size = Math.min(viewport.x, viewport.y) / 5
+
+  return (
+    <>
+      <div
+        className="absolute bg-gray-100"
+        style={{
+          width: `${size}px`,
+          height: `${size}px`,
+        }}
+      />
+    </>
+  )
 }
 
 export function App() {
   return (
     <div className="p-4 w-dvw h-dvh flex">
-      <ViewportContainer className="flex-1">
+      <ViewportContainer className="flex-1 relative">
         {(viewport) =>
           viewport ? <Canvas viewport={viewport} /> : null
         }
