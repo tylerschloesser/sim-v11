2a30bd794279d017c81292eb9e28862355fa44f1
Tyler Schloesser <tylerschloesser@gmail.com>
2024-11-30 17:03:50 -0800

    update algorithm

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,4 @@
+import invariant from 'tiny-invariant'
 import { z } from 'zod'
 import { ZVec2 } from './vec2'
 
@@ -15,7 +16,8 @@ export const Node = z.strictObject({
   id: z.string(),
   p: ZVec2,
   item: NodeItem.nullable(),
-  out: NodeRef.array(),
+  inputs: NodeRef.array(),
+  outputs: NodeRef.array(),
 })
 export type Node = z.infer<typeof Node>
 
@@ -27,27 +29,57 @@ const NODES = new Map<string, Node>()
       id: '0',
       p: { x: 0, y: 0 },
       item: { tick: 0 },
-      out: [{ id: '1' }],
+      inputs: [{ id: '3' }],
+      outputs: [{ id: '1' }],
     },
     {
       id: '1',
       p: { x: 1, y: 0 },
       item: null,
-      out: [{ id: '2' }],
+      inputs: [{ id: '0' }],
+      outputs: [{ id: '2' }],
     },
     {
       id: '2',
       p: { x: 1, y: 1 },
       item: null,
-      out: [{ id: '3' }],
+      inputs: [{ id: '1' }],
+      outputs: [{ id: '3' }],
     },
     {
       id: '3',
       p: { x: 0, y: 1 },
       item: null,
-      out: [{ id: '0' }],
+      inputs: [{ id: '2' }],
+      outputs: [{ id: '0' }],
     },
   ] satisfies Node[]
 ).forEach((node) => {
   NODES.set(node.id, node)
 })
+
+function step(nodes: Map<string, Node>) {
+  function refToNode({ id }: NodeRef) {
+    const node = nodes.get(id)
+    invariant(node)
+    return node
+  }
+
+  const seen = new Set<Node>()
+
+  function visit(node: Node) {
+    if (seen.has(node)) {
+      invariant(false, 'TODO handle cycle')
+    }
+    seen.add(node)
+    node.outputs.map(refToNode).forEach(visit)
+  }
+
+  for (const root of nodes.values()) {
+    if (!seen.has(root)) {
+      visit(root)
+    }
+  }
+}
+
+step(NODES)
