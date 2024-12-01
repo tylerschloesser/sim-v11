2abc8bbe4f5c9044343b3308a245559fe83fc3b1
Tyler Schloesser <tylerschloesser@gmail.com>
2024-11-30 18:51:04 -0800

    add cell

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,40 @@
+import { useState } from 'react'
 import { Vec2 } from './vec2'
 import { ViewportContainer } from './viewport-container'
+
+interface Cell {
+  p: Vec2
+}
 
 function Canvas({ viewport }: { viewport: Vec2 }) {
   const size = Math.min(viewport.x, viewport.y) / 5
 
+  const [cells] = useState(
+    () =>
+      [
+        {
+          p: new Vec2(0, 0),
+        },
+        {
+          p: new Vec2(1, 0),
+        },
+      ] satisfies Cell[],
+  )
+
   return (
     <>
-      <div
-        className="absolute border-2 border-white"
-        style={{
-          width: `${size}px`,
-          height: `${size}px`,
-        }}
-      />
+      {cells.map((cell, i) => (
+        <div
+          key={i}
+          className="absolute border-2 border-white"
+          style={{
+            width: `${size}px`,
+            height: `${size}px`,
+            left: `${cell.p.x * size}px`,
+            top: `${cell.p.y * size}px`,
+          }}
+        />
+      ))}
     </>
   )
 }
