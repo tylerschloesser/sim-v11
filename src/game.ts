6f1e07a7135b54408f446ab9b617a3d61187cb01
Tyler Schloesser <tylerschloesser@gmail.com>
2024-11-30 16:08:10 -0800

    add nodes

--- src/game.ts
+++ src/game.ts
@@ -1,20 +1,53 @@
 import { z } from 'zod'
 import { ZVec2 } from './vec2'
 
-const NodeRef = z.strictObject({
+export const NodeRef = z.strictObject({
   id: z.string(),
 })
-type NodeRef = z.infer<typeof NodeRef>
+export type NodeRef = z.infer<typeof NodeRef>
 
-const NodeItem = z.strictObject({
+export const NodeItem = z.strictObject({
   tick: z.number(),
 })
-type NodeItem = z.infer<typeof NodeItem>
+export type NodeItem = z.infer<typeof NodeItem>
 
-const Node = z.strictObject({
+export const Node = z.strictObject({
   id: z.string(),
   p: ZVec2,
   item: NodeItem.nullable(),
   out: NodeRef.array(),
 })
-type Node = z.infer<typeof Node>
+export type Node = z.infer<typeof Node>
+
+const nodes = new Map<string, Node>()
+
+;(
+  [
+    {
+      id: '0',
+      p: { x: 0, y: 0 },
+      item: { tick: 0 },
+      out: [{ id: '1' }],
+    },
+    {
+      id: '1',
+      p: { x: 1, y: 0 },
+      item: null,
+      out: [{ id: '2' }],
+    },
+    {
+      id: '2',
+      p: { x: 1, y: 1 },
+      item: null,
+      out: [{ id: '3' }],
+    },
+    {
+      id: '3',
+      p: { x: 0, y: 1 },
+      item: null,
+      out: [{ id: '0' }],
+    },
+  ] satisfies Node[]
+).forEach((node) => {
+  nodes.set(node.id, node)
+})
