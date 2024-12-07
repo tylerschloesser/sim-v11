5c51f00059fdffd1d1db7b31d639f0771e218aa9
Tyler Schloesser <tylerschloesser@gmail.com>
2024-12-07 08:00:30 -0800

    add tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
+import {
+  RouterProvider,
+  createRouter,
+} from '@tanstack/react-router'
 import { createRoot } from 'react-dom/client'
 import invariant from 'tiny-invariant'
-import { App } from './app'
 import './index.css'
+import { routeTree } from './routeTree.gen'
+
+const router = createRouter({ routeTree })
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 const container = document.getElementById('root')
 invariant(container)
 
-createRoot(container).render(<App />)
+createRoot(container).render(
+  <RouterProvider router={router} />,
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [TanStackRouterVite(), viteReact()],
 })
