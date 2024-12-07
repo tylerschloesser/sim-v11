a879b90a22c9ca80813599a1d7e2a80613a6b710
Tyler Schloesser <tylerschloesser@gmail.com>
2024-12-07 08:01:57 -0800

    cleanup root

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,17 +2,11 @@ import {
   Outlet,
   createRootRoute,
 } from '@tanstack/react-router'
-import * as React from 'react'
 
 export const Route = createRootRoute({
   component: RootComponent,
 })
 
 function RootComponent() {
-  return (
-    <React.Fragment>
-      <div>Hello "__root"!</div>
-      <Outlet />
-    </React.Fragment>
-  )
+  return <Outlet />
 }
