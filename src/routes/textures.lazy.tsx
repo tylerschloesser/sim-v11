631c15accd7b67bb8129bbc1059e5da7d3af15dd
Tyler Schloesser <tylerschloesser@gmail.com>
2024-12-08 13:53:57 -0800

    refactor texture component

--- src/routes/textures.lazy.tsx
+++ src/routes/textures.lazy.tsx
@@ -1,70 +1,12 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
-import { forwardRef, useCallback, useRef } from 'react'
+import { useCallback, useRef } from 'react'
 import invariant from 'tiny-invariant'
 import { renderSvgToImage, TextureId } from '../textures'
+import { Texture } from '../textures/texture'
 
 export const Route = createLazyFileRoute('/textures')({
   component: RouteComponent,
 })
-
-interface PlainTextureSvgProps {
-  color: string
-}
-
-const PlainTextureSvg = forwardRef<
-  SVGSVGElement,
-  PlainTextureSvgProps
->(({ color }, ref) => {
-  return (
-    <svg
-      ref={ref}
-      width={100}
-      height={100}
-      viewBox="0 0 100 100"
-    >
-      <rect width="100" height="100" fill={color} />
-    </svg>
-  )
-})
-PlainTextureSvg.displayName = 'PlainTextureSvg'
-
-interface TextureProps {
-  id: TextureId
-}
-
-const Texture = forwardRef<SVGSVGElement, TextureProps>(
-  ({ id }, ref) => {
-    switch (id) {
-      case TextureId.enum.NodeNormal:
-        return <PlainTextureSvg ref={ref} color="green" />
-      case TextureId.enum.NodeConsumer:
-        return <PlainTextureSvg ref={ref} color="blue" />
-      case TextureId.enum.NodeProducer:
-        return <PlainTextureSvg ref={ref} color="red" />
-      case TextureId.enum.NodeArrow:
-        return (
-          <svg
-            ref={ref}
-            width={100}
-            height={100}
-            viewBox="0 0 100 100"
-          >
-            <text
-              x="90"
-              y="50"
-              fontFamily="system-ui"
-              fontSize="30"
-              textAnchor="end"
-              dominantBaseline="middle"
-            >
-              {'→'}
-            </text>
-          </svg>
-        )
-    }
-  },
-)
-Texture.displayName = 'Texture'
 
 interface TextureSectionProps {
   id: TextureId
---
+++ src/textures/texture.tsx
@@ -0,0 +1,83 @@
+import { forwardRef } from 'react'
+import { TextureId } from '../textures'
+
+interface PlainTextureSvgProps {
+  color: string
+  textureId: TextureId
+}
+
+const PlainTextureSvg = forwardRef<
+  SVGSVGElement,
+  PlainTextureSvgProps
+>(({ color, textureId }, ref) => {
+  return (
+    <svg
+      data-texture-id={textureId}
+      ref={ref}
+      width={100}
+      height={100}
+      viewBox="0 0 100 100"
+    >
+      <rect width="100" height="100" fill={color} />
+    </svg>
+  )
+})
+PlainTextureSvg.displayName = 'PlainTextureSvg'
+
+export interface TextureProps {
+  id: TextureId
+}
+
+export const Texture = forwardRef<
+  SVGSVGElement,
+  TextureProps
+>(({ id }, ref) => {
+  switch (id) {
+    case TextureId.enum.NodeNormal:
+      return (
+        <PlainTextureSvg
+          ref={ref}
+          color="green"
+          textureId={id}
+        />
+      )
+    case TextureId.enum.NodeConsumer:
+      return (
+        <PlainTextureSvg
+          ref={ref}
+          color="blue"
+          textureId={id}
+        />
+      )
+    case TextureId.enum.NodeProducer:
+      return (
+        <PlainTextureSvg
+          ref={ref}
+          color="red"
+          textureId={id}
+        />
+      )
+    case TextureId.enum.NodeArrow:
+      return (
+        <svg
+          data-texture-id={id}
+          ref={ref}
+          width={100}
+          height={100}
+          viewBox="0 0 100 100"
+        >
+          <text
+            x="90"
+            y="50"
+            fontFamily="system-ui"
+            fontSize="30"
+            textAnchor="end"
+            dominantBaseline="middle"
+          >
+            {'→'}
+          </text>
+        </svg>
+      )
+  }
+})
+Texture.displayName = 'Texture'
