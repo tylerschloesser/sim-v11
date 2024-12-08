a751f9aab27419c34578e81d0475d02859264415
Tyler Schloesser <tylerschloesser@gmail.com>
2024-12-07 18:20:19 -0800

    refactor texture to svg

--- src/routes/textures.lazy.tsx
+++ src/routes/textures.lazy.tsx
@@ -1,36 +1,54 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
-import { Fragment, useEffect, useRef } from 'react'
-import invariant from 'tiny-invariant'
-import { renderTexture, TextureId } from '../textures'
+import { Fragment } from 'react'
+import { TextureId } from '../textures'
 
 export const Route = createLazyFileRoute('/textures')({
   component: RouteComponent,
 })
 
-interface TextureCanvasProps {
+interface PlainTextureSvgProps {
+  color: string
+}
+
+function PlainTextureSvg({ color }: PlainTextureSvgProps) {
+  return (
+    <svg width={100} height={100} viewBox="0 0 100 100">
+      <rect width="100" height="100" fill={color} />
+    </svg>
+  )
+}
+
+interface TextureProps {
   id: TextureId
 }
-function TextureCanvas({ id }: TextureCanvasProps) {
-  const canvas = useRef<HTMLCanvasElement>(null)
 
-  useEffect(() => {
-    invariant(canvas.current)
-    const context = canvas.current.getContext('2d')
-    invariant(context)
-    renderTexture(id, context)
-  }, [id])
-
-  return <canvas ref={canvas} width={100} height={100} />
+function Texture({ id }: TextureProps) {
+  switch (id) {
+    case TextureId.enum.NodeNormal:
+      return <PlainTextureSvg color="green" />
+    case TextureId.enum.NodeConsumer:
+      return <PlainTextureSvg color="blue" />
+    case TextureId.enum.NodeProducer:
+      return <PlainTextureSvg color="red" />
+    case TextureId.enum.NodeArrow:
+      return (
+        <div className="w-[100px] h-[100px] flex items-center justify-end p-2">
+          <span className="text-xl">&rarr;</span>
+        </div>
+      )
+  }
 }
 
 function RouteComponent() {
   return (
-    <div>
+    <div className="flex flex-col items-start">
       {[
         TextureId.options.map((id) => (
           <Fragment key={id}>
             <h2>{id}</h2>
-            <TextureCanvas id={id} />
+            <div className="border-2 border-gray-400 flex">
+              <Texture id={id} />
+            </div>
           </Fragment>
         )),
       ]}
