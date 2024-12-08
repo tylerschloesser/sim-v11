ee4e3405cbb4a7d594e90053bbceb02798bb6132
Tyler Schloesser <tylerschloesser@gmail.com>
2024-12-07 17:58:08 -0800

    add canvas

--- src/routes/textures.lazy.tsx
+++ src/routes/textures.lazy.tsx
@@ -1,9 +1,26 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { useEffect, useRef } from 'react'
+import invariant from 'tiny-invariant'
 
 export const Route = createLazyFileRoute('/textures')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/textures"!</div>
+  const canvas = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    invariant(canvas.current)
+    const context = canvas.current.getContext('2d')
+    invariant(context)
+
+    context.fillStyle = 'red'
+    context.fillRect(0, 0, 100, 100)
+  }, [])
+
+  return (
+    <div>
+      <canvas ref={canvas} width={100} height={100} />
+    </div>
+  )
 }
