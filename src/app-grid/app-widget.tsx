2cf3ea20560dc4d220c8314c2d45a6c5fe628843
Tyler Schloesser <tylerschloesser@gmail.com>
2024-12-27 21:01:55 -0600

    render producer rate

--- src/app-grid/app-widget.tsx
+++ src/app-grid/app-widget.tsx
@@ -1,7 +1,10 @@
 import clsx from 'clsx'
-import React, { useMemo } from 'react'
+import React, { useContext, useMemo } from 'react'
+import invariant from 'tiny-invariant'
 import { Vec2 } from '../common/vec2'
-import { CELL_SIZE } from './const'
+import { NodeType } from '../game/node'
+import { AppContext } from './app-context'
+import { CELL_SIZE, DEFAULT_PRODUCER_RATE } from './const'
 
 export interface AppWidgetProps {
   p: Vec2
@@ -16,6 +19,7 @@ export const AppWidget = React.forwardRef<
     const { x: tx, y: ty } = p.mul(CELL_SIZE)
     return `${tx}px ${ty}px`
   }, [p])
+  const producerRate = useProducerRate()
   return (
     <div
       ref={ref}
@@ -32,6 +36,26 @@ export const AppWidget = React.forwardRef<
       }}
     >
       ID: {id}
+      Producer Rate: {producerRate}
     </div>
   )
 })
+
+function useProducerRate(): number {
+  const { game } = useContext(AppContext)
+  return useMemo(() => {
+    const nodes = Object.values(game.nodes).filter(
+      (node) => node.type === NodeType.enum.Producer,
+    )
+    if (nodes.length === 0) {
+      return DEFAULT_PRODUCER_RATE
+    }
+    const first = nodes.at(0)!
+    invariant(
+      nodes
+        .slice(1)
+        .every((node) => node.rate === first.rate),
+    )
+    return first.rate
+  }, [game.nodes])
+}
--- src/app-grid/const.ts
+++ src/app-grid/const.ts
@@ -3,3 +3,5 @@ export const TICK_DURATION = 150
 
 // how far the user is allowed to drag before pointerup is considered a click
 export const DRAG_THRESHOLD_PX = 2
+
+export const DEFAULT_PRODUCER_RATE = 0.1
