904696aa783063b0d441e134d8eec9e303ae6d27
Tyler Schloesser <tylerschloesser@gmail.com>
2024-12-29 12:52:18 -0800

    implement widget consumer

--- src/app-grid/widget-consumer.tsx
+++ src/app-grid/widget-consumer.tsx
@@ -1,11 +1,20 @@
+import { Fragment } from 'react/jsx-runtime'
 import { ConsumerNode } from '../game/node'
 
 export interface WidgetConsumerProps {
   node: ConsumerNode
 }
-// @ts-expect-error
 export function WidgetConsumer({
   node,
 }: WidgetConsumerProps) {
-  return <div>TODO</div>
+  return (
+    <div className="grid grid-cols-[max-content_1fr] gap-1 font-mono">
+      {Object.entries(node.stats).map(([key, value]) => (
+        <Fragment key={key}>
+          <span>{key}</span>
+          <span>{value}</span>
+        </Fragment>
+      ))}
+    </div>
+  )
 }
