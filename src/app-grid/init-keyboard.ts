ddf67757dcc532a39afb3f5351f33d6463f6e07c
Tyler Schloesser <tylerschloesser@gmail.com>
2024-12-30 15:43:52 -0800

    refactor handle output direction

--- src/app-grid/init-keyboard.ts
+++ src/app-grid/init-keyboard.ts
@@ -20,77 +20,91 @@ export function initKeyboard({
   window.addEventListener(
     'keyup',
     (ev) => {
-      let direction: OutputDirection | null = null
       switch (ev.key) {
         case 'w':
         case 'ArrowUp':
-          direction = OutputDirection.enum.North
+          handleOutputDirection(
+            setGame,
+            view$,
+            OutputDirection.enum.North,
+          )
           break
         case 'a':
         case 'ArrowLeft':
-          direction = OutputDirection.enum.West
+          handleOutputDirection(
+            setGame,
+            view$,
+            OutputDirection.enum.West,
+          )
           break
         case 's':
         case 'ArrowDown':
-          direction = OutputDirection.enum.South
+          handleOutputDirection(
+            setGame,
+            view$,
+            OutputDirection.enum.South,
+          )
           break
         case 'd':
         case 'ArrowRight':
-          direction = OutputDirection.enum.East
-          break
-      }
-      if (!direction) {
-        return
-      }
-
-      let d: Vec2
-      switch (direction) {
-        case OutputDirection.enum.North:
-          d = new Vec2(0, -1)
-          break
-        case OutputDirection.enum.South:
-          d = new Vec2(0, 1)
-          break
-        case OutputDirection.enum.East:
-          d = new Vec2(1, 0)
-          break
-        case OutputDirection.enum.West:
-          d = new Vec2(-1, 0)
-          break
-      }
-
-      setGame((draft) => {
-        draft.updateType = null
-        if (!view$.value.hover) {
-          return
-        }
-
-        const inputId = toNodeId(view$.value.hover.p)
-        const input = draft.nodes[inputId]
-        if (!input) {
-          return
-        }
-
-        const outputId = toNodeId(new Vec2(input.p).add(d))
-        const output = draft.nodes[outputId]
-        if (!output) {
-          return
-        }
-
-        if (input.outputs[outputId]) {
-          delete input.outputs[outputId]
-        } else {
-          const result = connect(
-            draft.nodes,
-            inputId,
-            outputId,
+          handleOutputDirection(
+            setGame,
+            view$,
+            OutputDirection.enum.East,
           )
-          if (!result.success) {
-            console.error(result.errors)
-          }
-        }
-      })
+          break
+      }
     },
     { signal },
   )
+}
+
+function handleOutputDirection(
+  setGame: Updater<Game>,
+  view$: BehaviorSubject<AppView>,
+  direction: OutputDirection,
+): void {
+  let d: Vec2
+  switch (direction) {
+    case OutputDirection.enum.North:
+      d = new Vec2(0, -1)
+      break
+    case OutputDirection.enum.South:
+      d = new Vec2(0, 1)
+      break
+    case OutputDirection.enum.East:
+      d = new Vec2(1, 0)
+      break
+    case OutputDirection.enum.West:
+      d = new Vec2(-1, 0)
+      break
+  }
+
+  setGame((draft) => {
+    draft.updateType = null
+    if (!view$.value.hover) {
+      return
+    }
+
+    const inputId = toNodeId(view$.value.hover.p)
+    const input = draft.nodes[inputId]
+    if (!input) {
+      return
+    }
+
+    const outputId = toNodeId(new Vec2(input.p).add(d))
+    const output = draft.nodes[outputId]
+    if (!output) {
+      return
+    }
+
+    if (input.outputs[outputId]) {
+      delete input.outputs[outputId]
+    } else {
+      const result = connect(draft.nodes, inputId, outputId)
+      if (!result.success) {
+        console.error(result.errors)
+      }
+    }
+  })
 }
