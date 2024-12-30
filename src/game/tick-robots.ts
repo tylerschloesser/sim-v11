d15e48afb7c33bc9b4c0fdb7584621ba1ad53580
Tyler Schloesser <tylerschloesser@gmail.com>
2024-12-30 13:28:50 -0800

    return robots to terminal

--- src/game/tick-robots.ts
+++ src/game/tick-robots.ts
@@ -1,2 +1,21 @@
-// @ts-expect-error
-export function tickRobots(game: Game): void {}
+import { isEqual } from 'lodash-es'
+import invariant from 'tiny-invariant'
+import { Vec2 } from '../common/vec2'
+import { Game } from './game'
+
+export function tickRobots(game: Game): void {
+  for (const robot of Object.values(game.robots)) {
+    if (robot.jobId !== null) {
+      continue
+    }
+    const node = game.nodes[robot.nodeId]
+    invariant(node)
+    if (isEqual(robot.p, node.p)) {
+      robot.d = null
+      continue
+    }
+
+    robot.d = new Vec2(node.p).sub(new Vec2(robot.p))
+    robot.p = node.p
+  }
+}
